30376f75e4fcae844dc208578d8ac8a572b3c3d3
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-17 02:13:05 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,6 +5,7 @@ i = 0
   yield array[i]
     i = i + 1
   end
+  yield
 end
 
 # call your method here!
@@ -13,5 +14,4 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
-  yield
 end
